d69fcaaad3e1803ba2c9df8b54da059b86ff3013
Yavor K <ykonstantinov1@gmail.com>
2018-03-09 19:34:36 -0800

    Added in missing singleton class constructor and getInstance method to provide access to it

--- app/src/main/java/ApplicationLogic/ProgramSingletonController.java
+++ app/src/main/java/ApplicationLogic/ProgramSingletonController.java
@@ -11,13 +11,29 @@ public class ProgramSingletonController {
     private String userID;
     private JSONObject jsonResponse;
     private AccountApiInteractions currInstance;
+    private static ProgramSingletonController instance;
+
+    private  ProgramSingletonController(){
+        //Private instance to prevent anybody instantiating the singleton class without using official method
+    }
+
+    public static ProgramSingletonController getCurrInstantce(){
+        if(instance == null){
+            return new ProgramSingletonController();
+        }
+        else{
+            return instance;
+        }
+    }
 
 
-
+    //Creates a new user
     public void createNewUser(String name, String email, String password, Context appContext){
         currInstance = new AccountApiInteractions();
         currInstance.createNewUser(name, password, email, appContext);
     }
+
+    //Logs user into their account
     public void logIn(String email, String password, Context appContext){
         currInstance = new AccountApiInteractions();
         currInstance.userLogIn(email, password, appContext);
