691447562f138386428f34609ebe984cabfaf0b8
Cinnanut <rain.bowl@live.ca>
2018-03-12 23:14:05 -0700

    wrote base group server functions, need to test/implement

--- app/src/main/java/ApplicationLogic/AccountApiInteractions.java
+++ app/src/main/java/ApplicationLogic/AccountApiInteractions.java
@@ -28,6 +28,8 @@ public class AccountApiInteractions {
     private final String baseURL = "https://cmpt276-1177-bf.cmpt.sfu.ca:8443";
     private String bearerToken;
     private int userID = 0;
+    private int groupID = 0;
+    private JSONObject groupDetails;
 
     //Creates a single user using the inputs
 public void createNewUser(String userName, String userPassword, String userEmailAddr, Context appContext){
@@ -65,8 +67,8 @@ public void createNewUser(String userName, String userPassword, String userEmail
                 @Override
                 public void onError(ANError anError) {
                     Log.d(TAG, "onError:"+ jsonBody.toString());
-                    Log.d(TAG, "onError: "+anError.getErrorBody());
-                    Log.d(TAG, "onError: "+ anError.getErrorDetail().toString());
+                    Log.d(TAG, "onError: "+ anError.getErrorBody());
+                    Log.d(TAG, "onError: "+ anError.getErrorDetail());
 
                 }
             });
@@ -100,7 +102,7 @@ public void createNewUser(String userName, String userPassword, String userEmail
 
             @Override
             public void onError(ANError anError) {
-                Log.d(TAG, "onError: Error" + anError.getErrorDetail().toString());
+                Log.d(TAG, "onError: Error" + anError.getErrorDetail());
             }
         });
     }
@@ -144,6 +146,259 @@ public void createNewUser(String userName, String userPassword, String userEmail
     public int getUserID(){
         return userID;
     }
+
+
+
+    /*  classes for group implementation:    */
+
+    // create new group
+    public void createNewGroup(String groupDescription, int leaderID, Context appContext){
+        //Initialize androidNetworking library with current activity context
+        AndroidNetworking.initialize(appContext);
+        final JSONObject jsonBody = new JSONObject();
+        //Create the json body to be attached
+        try {
+            jsonBody.put("group description", groupDescription);
+            jsonBody.put("routeLatArray", new JSONArray());
+            jsonBody.put("routeLngArray", new JSONArray());
+            jsonBody.put("leader", leaderID);
+            jsonBody.put("memberUsers", new JSONArray());
+        }
+        catch (Exception e){
+            Log.d(TAG, "createNewGroup: Unexpected error");
+            e.printStackTrace();
+        }
+
+        //Make POST call to server with attached json body
+        AndroidNetworking.post(baseURL + "/groups")
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .addJSONObjectBody(jsonBody)
+                .build()
+                .getAsOkHttpResponseAndJSONObject(new OkHttpResponseAndJSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(Response okHttpResponse, JSONObject response) {
+                        try {
+                            groupID = response.getInt("id");
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "onResponse: Success " + okHttpResponse.code());
+                        Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError:"+ jsonBody.toString());
+                        Log.d(TAG, "onError: "+anError.getErrorBody());
+                        Log.d(TAG, "onError: "+ anError.getErrorDetail());
+                    }
+                });
+    }
+
+    //gets list of all groups
+    public void getGroupList(Context currContext){
+        AndroidNetworking.initialize(currContext);
+        AndroidNetworking.get(baseURL + "/groups")
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            groupDetails = response.getJSONObject("id");
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError: " + anError.getErrorBody());
+                        Log.d(TAG, "onError: " + anError.getErrorDetail());
+                    }
+                });
+    }
+
+    //gets group's details through groupID
+    public void getGroupDetails(int groupID, Context currContext){
+        AndroidNetworking.initialize(currContext);
+        AndroidNetworking.get(baseURL + "/groups/" + groupID)
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            groupDetails = response.getJSONObject("id");
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError: " + anError.getErrorBody());
+                        Log.d(TAG, "onError: " + anError.getErrorDetail());
+                    }
+                });
+    }
+
+
+    // update group
+    public void updateGroup(int groupID, final String newDescription, final double latitude, final double longitude, Context appContext){
+        //Initialize androidNetworking library with current activity context
+        AndroidNetworking.initialize(appContext);
+        AndroidNetworking.post(baseURL + "/groups/" + groupID)
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            response.put("group description", newDescription);
+                            response.put("routeLatArray", latitude);
+                            response.put("routeLngArray", longitude);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError: "+ anError.getErrorBody());
+                        Log.d(TAG, "onError: "+ anError.getErrorDetail());
+                    }
+                });
+    }
+
+    // delete group
+    public void deleteGroup(int groupID, Context appContext){
+        //Initialize androidNetworking library with current activity context
+        AndroidNetworking.initialize(appContext);
+        AndroidNetworking.delete(baseURL + "/groups/" + groupID)
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                Log.d(TAG, "error: JsonBody not deleted" + response.toString());
+            }
+            @Override
+            public void onError(ANError anError) {
+                Log.d(TAG, "onError errorCode : " + anError.getErrorCode());
+                Log.d(TAG, "onError: " + anError.getErrorBody());
+                Log.d(TAG, "onError: " + anError.getErrorDetail());
+            }
+        });
+    }
+
+    // get group members
+    public void getGroupMembers(int groupID, Context appContext){
+        //Initialize androidNetworking library with current activity context
+        AndroidNetworking.initialize(appContext);
+        AndroidNetworking.get(baseURL + "/groups/" + groupID + "/memberUsers")
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError: " + anError.getErrorBody());
+                        Log.d(TAG, "onError: " + anError.getErrorDetail());
+                    }
+                });
+    }
+
+    // add group member
+    public void addGroupMember(int groupID, final int memberID, Context appContext){
+        //Initialize androidNetworking library with current activity context
+        AndroidNetworking.initialize(appContext);
+        AndroidNetworking.post(baseURL + "/groups/" + groupID + "/memberUsers")
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (memberID < userID) {
+                                if ((Integer) response.get("id") != memberID) {
+                                    response.put("id", memberID);
+                                }
+                                else {
+                                    Log.d(TAG, "fail: user already in group" + response.toString());
+                                }
+                            }
+                            else {
+                                Log.d(TAG, "fail: user does not exist" + response.toString());
+
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError: " + anError.getErrorBody());
+                        Log.d(TAG, "onError: " + anError.getErrorDetail());
+                    }
+                });
+    }
+
+    // remove group member
+    public void removeGroupMember(int groupID, final int memberID, Context appContext){
+        //Initialize androidNetworking library with current activity context
+        AndroidNetworking.initialize(appContext);
+        AndroidNetworking.delete(baseURL + "/groups/" + groupID + "/memberUsers/" + memberID)
+                .addHeaders("apiKey", "F369E8E6-244B-4672-B8A8-1E44A32CA496")
+                .addHeaders("Authorization", bearerToken)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d(TAG, "onResponse: JsonBody " + response.toString());
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        Log.d(TAG, "error: member not deleted" + response.toString());
+                    }
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, "onError errorCode : " + anError.getErrorCode());
+                        Log.d(TAG, "onError: " + anError.getErrorBody());
+                        Log.d(TAG, "onError: " + anError.getErrorDetail());
+                    }
+                });
+    }
+
 
 
     //old class. Kept here in case shit hits the fan
--- app/src/main/java/ApplicationLogic/ProgramSingletonController.java
+++ app/src/main/java/ApplicationLogic/ProgramSingletonController.java
@@ -42,4 +42,63 @@ public class ProgramSingletonController {
     }
 
 
+
+    /* group functions */
+    /* need to implement/test */
+
+    //create new group
+    public void createNewGroup(String groupDescription, int leaderID, Context appContext){
+        currInstance = new AccountApiInteractions();
+        currInstance.createNewGroup(groupDescription, leaderID, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    //get group info
+    public void getGroupDetails(int groupID, Context appContext){
+        currInstance = new AccountApiInteractions();
+        currInstance.getGroupDetails(groupID, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    //get list of all groups
+    public void getGroupList(Context appContext){
+        currInstance = new AccountApiInteractions();
+        currInstance.getGroupList(appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    //update existing group info
+    public void updateGroup(int groupID, final String newDescription, final double latitude, final double longitude, Context appContext){
+        currInstance = new AccountApiInteractions();
+        currInstance.updateGroup(groupID, newDescription, latitude, longitude, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    // delete group
+    public void deleteGroup(int groupID, Context appContext) {
+        currInstance = new AccountApiInteractions();
+        currInstance.deleteGroup(groupID, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    // get group members
+    public void getGroupMembers(int groupID, Context appContext){
+        currInstance = new AccountApiInteractions();
+        currInstance.getGroupMembers(groupID, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    // add group member
+    public void addGroupMember(int groupID, final int memberID, Context appContext) {
+        currInstance = new AccountApiInteractions();
+        currInstance.addGroupMember(groupID, memberID, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
+
+    // remove group member
+    public void removeGroupMember(int groupID, final int memberID, Context appContext) {
+        currInstance = new AccountApiInteractions();
+        currInstance.removeGroupMember(groupID, memberID, appContext);
+        bearerToken = currInstance.getBearerToken();
+    }
 }
