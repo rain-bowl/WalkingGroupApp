953d221ae409c2af33d701d9f77ffa8abb3fb884
Yavor K <ykonstantinov1@gmail.com>
2018-03-15 21:50:38 -0700

    added custom alert dialog to be used for adding users

---
+++ app/src/main/java/com/example/nurdan/lavaproject/AddUserDialog.java
@@ -0,0 +1,40 @@
+package com.example.nurdan.lavaproject;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yavor on 15/03/18.
+ */
+
+public class AddUserDialog extends AppCompatDialogFragment{
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity())
+                .setTitle("Add a user to monitor");
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        dialogBuilder.setView(inflater.inflate(R.layout.add_user_dialog_layout, null))
+                .setPositiveButton("Add user", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return dialogBuilder.create();
+    }
+}
