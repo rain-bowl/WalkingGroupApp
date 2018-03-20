5f5c942ff55abe96ed1daace533070570937f476
Yavor K <ykonstantinov1@gmail.com>
2018-03-20 13:04:46 -0700

    added functioning toolbar for navigation

--- app/src/main/java/UIFragmentClasses/userProfileDisplayFragment.java
+++ app/src/main/java/UIFragmentClasses/userProfileDisplayFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nurdan.lavaproject.R;
 
--- app/src/main/java/com/example/nurdan/lavaproject/UserProfile.java
+++ app/src/main/java/com/example/nurdan/lavaproject/UserProfile.java
@@ -6,16 +6,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;;
 
+import UIFragmentClasses.UserProfileEditInfoFragment;
 import UIFragmentClasses.userProfileDisplayFragment;
 
 public class UserProfile extends AppCompatActivity {
+    MenuItem editItem, backItem, mainMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        setUpToolbar();
         loadFragment(new userProfileDisplayFragment());
+
     }
 
     public void loadFragment(Fragment fragmentClass){
@@ -23,6 +32,40 @@ public class UserProfile extends AppCompatActivity {
         fTInstance.replace(R.id.userProfileFragmentContainer, fragmentClass);
         fTInstance.commit();
 
+    }
+
+    private void setUpToolbar(){
+        Toolbar profileToolbar = (Toolbar) findViewById(R.id.profileDisplayToolbar);
+        setSupportActionBar(profileToolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_display_menu, menu);
+         editItem = menu.findItem(R.id.editItem);
+        backItem = menu.findItem(R.id.backItem);
+        mainMenuItem = menu.findItem(R.id.mainMenuItem);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.editItem:
+                backItem.setVisible(true);
+                editItem.setVisible(false);
+                loadFragment(new UserProfileEditInfoFragment());
+                break;
+            case R.id.mainMenuItem:
+                Intent mainMenu = MainMenu.mainMenuIntent(getApplicationContext());
+                startActivity(mainMenu);
+                break;
+            case R.id.backItem:
+                loadFragment(new userProfileDisplayFragment());
+        }
+        return true;
     }
 
     public static Intent userProfileIntent(Context activityContexts){
