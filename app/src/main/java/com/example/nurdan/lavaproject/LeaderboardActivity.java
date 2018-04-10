2c55bcb91ef193c98bd29ac9af0fb02dbd721973
Yavor Konstantinov <ykonstantinov1@gmail.com>
2018-04-09 20:15:44 -0700

    Implemented the leaderboard

--- app/src/main/java/com/example/nurdan/lavaproject/LeaderboardActivity.java
+++ app/src/main/java/com/example/nurdan/lavaproject/LeaderboardActivity.java
@@ -10,20 +10,33 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.gson.JsonObject;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+import ApplicationLogic.LeaderboardMember;
 import ApplicationLogic.ProgramSingletonController;
 
 public class LeaderboardActivity extends AppCompatActivity {
     private ProgramSingletonController currInstance = ProgramSingletonController.getCurrInstance();
     private JSONArray userlist;
+    private int leaderboardSize;
+    private ArrayList<LeaderboardMember> userAccount;
+    private ArrayList<String> userNames;
+    private ListView leaderboardDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+        leaderboardDisplay = findViewById(R.id.leaderboard);
+        getUsers getAllUsers = new getUsers();
+        getAllUsers.execute();
 
         makeBackBtn();
     }
@@ -37,13 +50,44 @@ public class LeaderboardActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void v) {
-
+            if(userlist.length() < 50){
+                leaderboardSize = userlist.length();
+            }
+            else{
+                leaderboardSize = 50;
+            }
+            try{
+                //Iterate through all users to retrieve their information
+                for (int i = 0; i < leaderboardSize; i++){
+                    JSONObject memberJSON = userlist.getJSONObject(i);
+                    //Create a new member and populate fields
+                    LeaderboardMember member = new LeaderboardMember();
+                    member.setFirstName(memberJSON.getString("name"));
+                    member.setUserPoints(memberJSON.getInt("totalPointsEarned"));
+                    //Add to the user list
+                    userAccount.add(member);
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            populateLeaderboard(leaderboardDisplay);
         }
     }
 
     // set up leaderboard listview
-    private void populateLeaderboard(ListView list, ArrayList<String> names){
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, names);
+    private void populateLeaderboard(ListView list){
+        //Sort the arraylist of all users
+        Collections.sort(userAccount, new Comparator<LeaderboardMember>() {
+            @Override
+            public int compare(LeaderboardMember o1, LeaderboardMember o2) {
+               return Integer.valueOf(o1.getUserPoints()).compareTo(Integer.valueOf(o2.getUserPoints()));
+            }
+        });
+        for(int i = 0; i < userAccount.size(); i++){
+            userNames.add(userAccount.get(i).getFirstName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, userNames);
         list.setAdapter(adapter);
     }
 
